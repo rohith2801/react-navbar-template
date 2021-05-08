2ce9a30a9f47ce46215311ae8cbe699125cd35a0
Rohith Kumar K <rohith2801@Rohiths-MacBook-Pro.local>
2021-05-08 16:31:46 +0530

    initial structure

---
+++ src/templates/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import SideMenu from './SideMenu';
+
+const Layout = () => {
+    return <>
+        <Header />
+        <SideMenu />
+    </>;
+};
+
+export default Layout;
---
+++ src/templates/SideMenu.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SideMenu = () => {
+    return <>SideMenu</>;
+};
+
+export default SideMenu;
